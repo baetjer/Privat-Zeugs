abaff5526fd45ca324dc1b71109df03656ddff9d
O.Bätjer <79996046+baetjer@users.noreply.github.com>
2021-04-02 22:35:14 +0200

    Create Program.cs

---
+++ Torili/Torili/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torili
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Torili();
+
+        public void 
+}
