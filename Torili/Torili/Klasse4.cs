3243ddada8e59bf916e85f1bb015127a2ede91c6
O.Bätjer <79996046+baetjer@users.noreply.github.com>
2021-04-08 11:19:02 +0200

    Create Klasse4.cs

---
+++ Torili/Torili/Klasse4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vererbung
+{
+    public class Klasse4
+    {
+        //String[] liste = new String[2];
+
+        String[] liste = new string[2] {"Innensechskant", "Sechskant"};
+
+        public void Array()
+        {
+            foreach (string i in liste)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        //public string Liste { get => liste; set => liste = value; }
+    }
+}
