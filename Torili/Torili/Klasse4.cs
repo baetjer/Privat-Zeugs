361872f03dfc88452db6ed72551ad0845bc44f13
O.Bätjer <79996046+baetjer@users.noreply.github.com>
2021-04-08 12:26:49 +0200

    Update Klasse4.cs

--- Torili/Torili/Klasse4.cs
+++ Torili/Torili/Klasse4.cs
@@ -9,17 +9,29 @@ namespace Vererbung
     public class Klasse4
     {
         //String[] liste = new String[2];
+        int zahl1;
 
-        String[] liste = new string[2] {"Innensechskant", "Sechskant"};
+        string[] liste = new string[2] {"Innensechskant", "Sechskant"};
+
+        //public void Array_Liste(string[] _liste)
+        //{
+        //    liste = _liste;
+        //}
+
+        //public string[] Liste { get => liste; set => liste = value; }
 
         public void Array()
         {
             foreach (string i in liste)
             {
-                Console.WriteLine(i);
+                Console.WriteLine(zahl1 + " " + i);
+                zahl1 += 1;
             }
         }
 
-        //public string Liste { get => liste; set => liste = value; }
+        public string getElement(int i)
+        {
+            return liste[i];
+        }
     }
 }
